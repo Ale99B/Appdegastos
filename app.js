2a4d5045f1d6d99cfa6895a3376644e4155788b9
velas eunoia <baez2385@gmail.com>
2023-09-06 11:09:01 -0300

    variable acumuladora

--- app.js
+++ app.js
@@ -15,42 +15,39 @@ class Gestion{
     }
 }
 
-class GestionDeProductos{
-    constructor(){
-        this.gestionarListado = []
+class GestionDeProductos {
+    constructor() {
+        this.gestionarListado = [];
     }
 
     agregar(productoSemanal) {
-        this.gestionarListado.push(productoSemanal)
+        this.gestionarListado.push(productoSemanal);
     }
 
     presentar() {
-        let ordenDeProductos = ""
-        this.gestionarListado.forEach ( productoSemanal => {
-            // alert(productoSemanal.nombre+" "+productoSemanal.precio)
-            ordenDeProductos =  ordenDeProductos + " Nombre: " +productoSemanal.nombre+ " Precio: "+productoSemanal.precio+"\n"
-            return ordenDeProductos
-        })
+        let ordenDeProductos = "";
+        this.gestionarListado.forEach(productoSemanal => {
+            ordenDeProductos += " Nombre: " + productoSemanal.nombre + " Precio: " + productoSemanal.precio + "\n";
+        });
+        return ordenDeProductos; // Mover el return aquí
     }
 }
 
+const p1 = new ProductoSemanal("alimentos", 5000);
+const p2 = new ProductoSemanal("higiene", 6000);
+const p3 = new ProductoSemanal("limpieza", 3000);
+const p4 = new ProductoSemanal("decoracion", 6500);
+const p5 = new ProductoSemanal("ropa", 9000);
+const p6 = new ProductoSemanal("extras", 7000);
 
+const reguladorDeGestion = new GestionDeProductos();
 
-const p1 = new ProductoSemanal("alimentos",5000)
-const p2 = new ProductoSemanal ("higiene",6000)
-const p3 = new ProductoSemanal ("limpieza",3000)
-const p4 = new ProductoSemanal ("decoracion",6500)
-const p5 = new ProductoSemanal ("ropa",9000)
-const p6 = new ProductoSemanal ("extras",7000)
+reguladorDeGestion.agregar(p1);
+reguladorDeGestion.agregar(p2);
+reguladorDeGestion.agregar(p3);
+reguladorDeGestion.agregar(p4);
+reguladorDeGestion.agregar(p5);
+reguladorDeGestion.agregar(p6);
 
-const reguladorDeGestion = new GestionDeProductos()
-
-reguladorDeGestion.agregar(p1)
-reguladorDeGestion.agregar(p2)
-reguladorDeGestion.agregar(p3)
-reguladorDeGestion.agregar(p4)
-reguladorDeGestion.agregar(p5)
-reguladorDeGestion.agregar(p6)
-
-alert( reguladorDeGestion.presentar() )
+alert(reguladorDeGestion.presentar());
 
