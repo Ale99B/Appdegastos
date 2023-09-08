f7355f7b424d8ae2e683b389cec4968e808b633b
velas eunoia <baez2385@gmail.com>
2023-09-08 11:25:12 -0300

    objectos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// alert("Bienvenido a la App de gestion de gastos")
+alert("Bienvenido a la App de gestion de gastos")
 
 
 class ProductoSemanal{
@@ -6,12 +6,44 @@ class ProductoSemanal{
         this.nombre = nombre
         this.precio = precio
         this.descuento = descuento
+        this.cantidad = 0
+    }
+
+    agregarCantidad(cantidadDeseada){
+        this.cantidad = this.cantidad + cantidadDeseada
+    }
+
+    detalle(){
+        return "    Nombre: " + this.nombre + " Precio: $" + this.precio + "\n";
+    }
+
+    detalleGestionCarrito(){
+        return "    Nombre: " + this.nombre + " Precio: $" + this.precio + "Cantidad: "+this.cantidad+ "\n";
     }
 }
 
-class Gestion{
+class GestionCarrito{
     constructor(){
-        this.listaGestion = []
+        this.listaGestionCarrito = []
+    }
+
+    agregar(productoSemanalNuevo){
+        let subsiste = this.gestionCarrito.some(productoSemanal => productoSemanal.nombre == productoSemanalNuevo.nombre)
+        if(!subsiste){
+            this.gestionCarrito.push(productoSemanalNuevo)
+        }
+    }
+
+    mostrar(){
+        let ordenDeCarrito = "Carrito: \n\n";
+        this.gestionCarrito.forEach(productoSemanal => {
+            ordenDeCarrito += productoSemanal.detalleGestionCarrito()
+        });
+        return ordenDeCarrito;
+    }
+
+    calcularTotal(){
+        return this.listaGestionCarrito.reduce( (cantidad, producto => cantidad + producto.precio * producto.cantidad, 0 ))
     }
 }
 
@@ -25,21 +57,26 @@ class GestionDeProductos {
     }
 
     presentar() {
-        let ordenDeProductos = "";
+        let ordenDeProductos = "Por favor, tenga presente el nombre del producto que desea añadir: \n\n";
         this.gestionarListado.forEach(productoSemanal => {
-            ordenDeProductos += " Nombre: " + productoSemanal.nombre + " Precio: " + productoSemanal.precio + "\n";
+            ordenDeProductos += productoSemanal.detalle()
         });
-        return ordenDeProductos; // Mover el return aquí
+        return ordenDeProductos;
+    }
+
+    explorar(nombre) {
+        return this.gestionarListado.find(productoSemanal => productoSemanal.nombre == nombre)
     }
 }
 
-const p1 = new ProductoSemanal("alimentos", 5000);
-const p2 = new ProductoSemanal("higiene", 6000);
-const p3 = new ProductoSemanal("limpieza", 3000);
-const p4 = new ProductoSemanal("decoracion", 6500);
-const p5 = new ProductoSemanal("ropa", 9000);
-const p6 = new ProductoSemanal("extras", 7000);
+const p1 = new ProductoSemanal("Alimentos", 5000);
+const p2 = new ProductoSemanal("Higiene", 6000);
+const p3 = new ProductoSemanal("Limpieza", 3000);
+const p4 = new ProductoSemanal("Decoracion", 6500);
+const p5 = new ProductoSemanal("Ropa", 9000);
+const p6 = new ProductoSemanal("Extras", 7000);
 
+const gestionCarrito = new GestionCarrito();
 const reguladorDeGestion = new GestionDeProductos();
 
 reguladorDeGestion.agregar(p1);
@@ -49,5 +86,19 @@ reguladorDeGestion.agregar(p4);
 reguladorDeGestion.agregar(p5);
 reguladorDeGestion.agregar(p6);
 
-alert(reguladorDeGestion.presentar());
+let aclaracion = ""
 
+do{
+    alert(reguladorDeGestion.presentar());
+    let nombre = prompt("Ingrese el nombre del producto que desea añadir.")
+    const productoSemanal =reguladorDeGestion.explorar(nombre)
+    let cantidadDeseada = Number.parseInt(prompt("Ingrese el cantidad que desea"))
+    productoSemanal.cantidadDeseada(cantidadDeseada)
+    gestionCarrito.agregar(productoSemanal)
+    alert(gestionCarrito.detalleGestionCarrito())
+
+    aclaracion = prompt("Escriba 'SI', si desea terminar de añadir productos").toLowerCase()
+}while(aclaracion != SI)
+
+
+alert("El total es de $"+gestionCarrito.calcularTotal())
